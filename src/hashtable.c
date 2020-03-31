040c8440660fb70c4bdc64406b6fac251149d256
yoav <yoav.shalev67@gmail.com>
2020-03-31 11:13:57 +0300

    adding hashCode function

--- src/hashtable.c
+++ src/hashtable.c
@@ -7,23 +7,46 @@ file: hashtable.c
 #include "../include/assembler.h"
 #include "../include/hashtable.h"
 
-Hashptr hase_table;
+extern HashTable* hash_table;
 
-void init_data()
+/*
+int insert(char* lable, int adrs_type)
+input: lable - pointer to lable
+adrs_type - the addressing type in the node
+*/
+void insert(char* lable, int adrs_type)
 {
-	hase_table = (Hashptr)malloc(sizeof(HashTable));
-	strcpy(&hase_table->lable, DEF_LABLE);
-	hase_table->plist = NULL;
-}
-int insert()
-{
-	return 0;
-}
-void clear()
-{
+	/*HashTable* phash = hash_table;*/
+	node* newNode = NULL;
 
-}
-void print_HashTable()
-{
+	/*go throgh the hash table and chech if there is al*/
+	while(strcmp(hash_table->lable, lable) != 0 && (hash_table->next != NULL))
+		hash_table = hash_table->next;
 
+	newNode = (node*)malloc(sizeof(node));
+
+	if(newNode == NULL)
+	{
+		printf("Error: memory not allocated to newNode");
+		return;
+	}
+
+	newNode->addr = IC;
+	newNode->type = adrs_type;
+	IC++;
+	newNode->next = hash_table->plist;
+	hash_table->plist = newNode;
+}
+
+int hashCode(char key[])
+{
+	int hash = 0;
+	int index = 0;
+	int i = 0;
+
+	while(key[i])
+		hash+=key[i];
+	index = hash % HASH_SIZE;
+
+	return index;
 }
