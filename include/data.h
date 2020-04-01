9ab82ca964418fbd8ac8d1d83a19240067b495e1
yoav <yoav.shalev67@gmail.com>
2020-04-01 22:19:53 +0300

    deleteing all func

--- include/data.h
+++ include/data.h
@@ -6,8 +6,5 @@ file: data.h
 #ifndef _DATA_H
 #define _DATA_H
 
-void init_data();
-void add_lable(char* lable, int adrs_type);
-void define_lable(char lable[], int counter_type, int def_type);
 
 #endif
--- src/data.c
+++ src/data.c
@@ -6,40 +6,4 @@ file: data.c
 
 #include "../include/assembler.h"
 #include "../include/data.h"
-#include "../include/hashtable.h"
-
-/*
-void init_data() 
-initialize variables and the Hash_Table
-called in the beginning of processing a new file.
-*/
-
-void init_data()
-{
-	hash_table = (HashTable*)malloc(sizeof(HashTable));
-	if(hash_table == NULL)
-	{
-		printf("Error: memory not allocated to Hash Table");
-		return;
-	}
-
-	strcpy(hash_table->lable, NULL_LBL);
-    hash_table->plist = NULL;
-    IC = DC = 0;
-	caught_error = FALSE;
-}
-
-void add_lable(char* lable, int adrs_type)
-{
-	insert(lable, adrs_type);
-	if(strcmp(hash_table->lable, lable)) /*if no record of the lable was found*/
-	{
-		define_lable(lable, DEF_CODE, DEF_NDS);
-	}
-}
-
-void define_lable(char lable[], int counter_type, int def_type)
-{
-	
-}
 
