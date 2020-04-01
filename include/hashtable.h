644067b7d899fe2e0778a56b89d76016f21f2e5b
yoav <yoav.shalev67@gmail.com>
2020-04-01 22:28:36 +0300

    adding 2 function

--- include/hashtable.h
+++ include/hashtable.h
@@ -6,19 +6,20 @@ file: hashtable.h
 #ifndef _HASHTABLE_H
 #define _HASHTABLE_H
 
-#define HASH_SIZE 128
+#define HASH_SIZE 64
 
-/*Node for storing an item in a linked list*/
-typedef struct node
+/*Defines a node in each in a labl's occurrence list*/
+typedef struct occurrence
 {
 	int addr;
 	int type;  /*states in what kind of addressing method the lable was used*/
-	struct node* next;
-}node;
+	struct occurrence* next;
+}occ;
 
-typedef struct HashTable
+/*Defines a node in the labels' list
+  each node holds general information about the label as described*/
+typedef struct node
 {
-	char lable[32];  /*lable name*/
 	int type;        /*notes if the lable points to data or code*/
 
 	/*linking information type: 'E'xternal, 'A'bsolute or 'R'elocatable*/
@@ -27,15 +28,29 @@ typedef struct HashTable
 	/*raw addressing value before addition of starting 
 	address value and optional addition for data labels*/
 	int addr_value;
-	node* plist;     /*pointer to list*/
-	struct HashTable* next; /*point to the struct in the HashTable*/
+	occ* pocc;         /*pointer to list of occurrence*/
+
+	struct node* next; /*point to the next node in the list*/
+}node;
+
+typedef struct HashTable
+{
+	char label[LBL_MAX + 1];  /*the key of the value in the array*/
+
+	node* value;              /*the value is a struct node*/
 }HashTable;
 
+/*Each cell of this array is a pointer to the first node of a linked list, 
+because we want our HashTable to use a chaining collision handling*/
 
-HashTable* hash_table;    /*pointer to Symbol Table*/
+HashTable* hash_table[HASH_SIZE];    /*pointer to Symbol Table*/
 
-void insert(char* lable, int adrs_type);
+void insert(char key[], node* value);
+node* get(char key[]);
+int contains(char key[]);
 int hashCode(char key[]);
-
+node* add_lable_occ(char* lable, int adrs_type);
+node* define_lable(char lable[], int counter_type, int def_type);
+void init_data();
 
 #endif
--- src/hashtable.c
+++ src/hashtable.c
@@ -6,43 +6,30 @@ file: hashtable.c
 
 #include "../include/assembler.h"
 #include "../include/hashtable.h"
+#include "../include/util.h"
 
-extern HashTable* hash_table;
+extern HashTable* hash_table[HASH_SIZE];
 
 /*
-int insert(char* lable, int adrs_type)
-input: lable - pointer to lable
-adrs_type - the addressing type in the node
+void init_data() 
+initialize variables and the Hash_Table
+called in the beginning of processing a new file.
 */
-void insert(char* lable, int adrs_type)
+void init_data()
 {
-	/*HashTable* phash = hash_table;*/
-	node* newNode = NULL;
+	int i;
 
-	/*go throgh the hash table and chech if there is al*/
-	while(strcmp(hash_table->lable, lable) != 0 && (hash_table->next != NULL))
-		hash_table = hash_table->next;
+	for(i = 0; i < HASH_SIZE; i++)
+		hash_table[i]->value = NULL;
 
-	newNode = (node*)malloc(sizeof(node));
-
-	if(newNode == NULL)
-	{
-		printf("Error: memory not allocated to newNode");
-		return;
-	}
-
-	newNode->addr = IC;
-	newNode->type = adrs_type;
-	IC++;
-	newNode->next = hash_table->plist;
-	hash_table->plist = newNode;
+	strcpy(hash_table[0]->label, NULL_LBL);
+    IC = DC = 0;
+	caught_error = FALSE;
 }
 
 int hashCode(char key[])
 {
-	int hash = 0;
-	int index = 0;
-	int i = 0;
+	int hash = 0, index = 0, i = 0;
 
 	while(key[i])
 		hash+=key[i];
@@ -50,3 +37,104 @@ int hashCode(char key[])
 
 	return index;
 }
+
+/*
+void insert(int key, node value)
+input: key - the key of the value in the array
+       value - the value is a struct node
+*/
+void insert(char key[], node* value)
+{
+	int i;
+	if(contains(key))
+	{
+		printf("Error: %s already exists!\n", key);
+		return;
+	}
+
+	i = hashCode(key);
+	strcpy(hash_table[i]->label, key);
+	hash_table[i]->value = value;
+}
+
+int contains(char key[])
+{
+	int i;
+	char* curlabel = NULL;
+	strcpy(curlabel, hash_table[hashCode(key)]->label);
+
+	for(i = 0; i < HASH_SIZE; i++)
+	{
+		if(strcmp(hash_table[i]->label, curlabel))
+			return TRUE;
+	}
+	printf("Error: %s not found!\n", curlabel);
+	return FALSE;
+}
+
+node* get(char key[])
+{
+	int i;
+	char* curlabel = NULL;
+	strcpy(curlabel, hash_table[hashCode(key)]->label);
+
+	for(i = 0; i < HASH_SIZE; i++)
+	{
+		if(strcmp(hash_table[i]->label, curlabel))
+			return hash_table[i]->value;
+	}
+
+		printf("Error: %s not found!\n", curlabel);
+		return NULL;
+}
+
+node* define_lable(char lable[], int type, int def_type)
+{
+	node* newnode = NULL;
+
+	trim_str(lable);
+
+	if(contains(lable))
+	{
+		caught_error = TRUE;
+		fprintf(stderr, "Error: in line %d!\n%shas multiple definitions.\n", line_num, lable);
+		return NULL;
+	}
+	/*no record was found for the label*/
+
+	newnode = (node*)malloc(sizeof(node));
+	newnode->type = type;
+	newnode->defined_as = def_type;
+	if(type == CODE_DEF)
+		newnode->addr_value = IC;
+	if(type == DATA_DEF)
+		newnode->addr_value = DC;
+
+	newnode->pocc = NULL;
+	newnode->next = NULL;
+
+	return newnode;
+}
+
+node* add_lable_occ(char* lable, int adrs_type)
+{
+	node* newnode = NULL;
+	occ* newocc;
+
+	if(contains(lable)) /*if no record of the label was found*/
+		newnode = define_lable(lable, CODE_DEF, DEF_OCC);
+
+	/*symbol is already defined, create an occurrence*/
+	newocc = (occ*)malloc(sizeof(occ));
+	newocc->addr = IC;
+	newocc->type = adrs_type;
+	IC++;
+	newocc->next = NULL;
+
+	newnode->pocc = newocc;
+	newnode->next = NULL;
+
+	return newnode;
+}
+
+
